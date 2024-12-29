b98b37697a7d4496fac8b44cd6aa574fcc480872
Athena65 <burkis_@hotmail.com>
2024-12-29 23:46:48 +0300

    hepsiburada ve trendyol dan urunleri degerlendirmeleri alinabiliyor , kod daha okunur halde.

--- get_global_rating_of_product.py
+++ get_global_rating_of_product.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup
 # URLS to fetch datas
 BASE_URLS = [
     "https://www.trendyol.com/",
-    #"https://www.n11.com/"
+    "https://www.hepsiburada.com/"
 ]
 
 def get_chrome_options():
@@ -17,61 +17,70 @@ def get_chrome_options():
     Chrome için gerekli Selenium ayarlarını döndürür.
     """
     chrome_options = Options()
-    chrome_options.add_argument('--headless')  # Arka planda çalıştır
-    chrome_options.add_argument('--disable-gpu')  # GPU'yu devre dışı bırak
-    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')  # Bellek sorunları için
-    chrome_options.add_argument("start-maximized")
-    chrome_options.add_argument("enable-automation")
-    chrome_options.add_argument("--disable-infobars")
-    chrome_options.add_argument("window-size=1920,1080")
-    chrome_options.add_argument('--disable-browser-side-navigation')
-    chrome_options.add_argument('--disable-extensions')
+    arguments = [
+        '--headless',
+        '--disable-gpu',
+        '--disable-blink-features=AutomationControlled',
+        '--no-sandbox',
+        '--disable-dev-shm-usage',
+        'start-maximized',
+        'enable-automation',
+        '--disable-infobars',
+        'window-size=1920,1080',
+        '--disable-browser-side-navigation',
+        '--disable-extensions',
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+    ]
+    for arg in arguments:
+        chrome_options.add_argument(arg)
     chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
     chrome_options.add_experimental_option('useAutomationExtension', False)
-
-    # Gerçekçi bir User-Agent
-    chrome_options.add_argument(
-        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
-
     return chrome_options
 
-def get_product_url(base_url, product_name):
-    """Ürün adını arayıp ürün detay sayfasının URL'sini alır."""
 
-    # Arama URL'si
+def get_search_urls(base_url, product_name):
+    """
+    Arama URL'sini belirler.
+    """
     if "trendyol" in base_url:
-        """Trendyol'da ürün adını arayıp arama sonucu ilk ürünün detay sayfasının URL'sini alır."""
-        search_url = f"{base_url.strip('/')}/sr?q={product_name.replace(' ', '%20')}"
-        product_class = 'p-card-wrppr'
-    #elif "n11" in base_url:
-    #     search_url = f"{base_url}arama?q={product_name.replace(' ', '+')}"
-    #     product_class = 'pro'
+        return f"{base_url.strip('/')}/sr?q={product_name.replace(' ', '%20')}"
+    elif "hepsiburada" in base_url:
+        return f"{base_url.strip('/')}/ara?q={product_name.replace(' ', '+')}"
     else:
         raise Exception("Desteklenmeyen bir URL")
 
-    # Selenium ayarları
-    chrome_options = get_chrome_options()
+def get_product_detailed_link(driver, base_url, search_url):
+    """
+    Ürün detay sayfasının linkini döndürür.
+    """
+    driver.get(search_url)
+    try:
+        # Cloudflare veya sayfa yükleme engellerini aşmak için bekle
+        time.sleep(10)  # Sayfanın tamamen yüklenmesi için sabit bekleme
 
-    driver = webdriver.Chrome(chrome_options)
+        # Trendyol veya Hepsiburada'ya göre uygun linki bul
+        link_element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "div.p-card-wrppr a" if "trendyol" in base_url else "a.moria-ProductCard-gyqBb"))
+        )
+
+        if link_element:
+            return link_element.get_attribute('href')
+        else:
+            raise Exception("Ürün linki bulunamadı.")
+    except Exception as e:
+        raise Exception(f"Ürün linki bulunamadı: {str(e)}")
+
+def get_product_url(base_url, product_name):
+    """Ürün adını arayıp ürün detay sayfasının URL'sini alır."""
+    search_url = get_search_urls(base_url, product_name)
+    chrome_options = get_chrome_options()
+    driver = webdriver.Chrome(options=chrome_options)
     driver.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument', {
         'source': "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})"
     })
 
-    driver.get(search_url)
-
     try:
-        # Cloudflare için bekleme
-        time.sleep(10)  # Sayfanın tamamen yüklenmesi için sabit bekleme
-
-        # Öğeyi bekle
-        link_element = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "div.p-card-wrppr a"))
-        )
-        product_link = link_element.get_attribute('href')
-    except Exception as e:
-        raise Exception(f"Ürün linki bulunamadı: {str(e)}")
+        product_link = get_product_detailed_link(driver, base_url, search_url)
     finally:
         driver.quit()
 
@@ -83,7 +92,8 @@ def get_product_url(base_url, product_name):
     else:
         raise Exception("Geçerli bir ürün linki bulunamadı.")
 
-def get_product_html(product_url):
+
+def get_product_detailed_page_html(product_url):
     """
     Ürün detay sayfasının HTML içeriğini döndürür.
     """
@@ -103,18 +113,25 @@ def get_product_html(product_url):
 
     return html_content
 
-def extract_rating_from_html(html_content):
+def extract_rating_from_html(html_content, base_url):
     """
     HTML içeriğinden "product-rating-score" sınıfındaki değeri çıkarır.
     """
     soup = BeautifulSoup(html_content, 'html.parser')
 
     # Rating değerini içeren elementi bul
-    rating_element = soup.find('div', class_='product-rating-score')
-    if rating_element:
-        value_element = rating_element.find('div', class_='value')
-        if value_element:
-            return value_element.text.strip()
+    if "trendyol" in base_url:
+        # Trendyol için "product-rating-score" sınıfındaki değeri al
+        rating_element = soup.find('div', class_='product-rating-score')
+        if rating_element:
+            value_element = rating_element.find('div', class_='value')
+            if value_element:
+                return value_element.text.strip()
+    elif "hepsiburada" in base_url:
+        # Hepsiburada için "JYHIcZ8Z_Gz7VXzxFB96" sınıfındaki değeri al
+        rating_element = soup.find('span', class_='JYHIcZ8Z_Gz7VXzxFB96')
+        if rating_element:
+            return rating_element.text.strip()
 
     return "Not Found"
 
@@ -128,23 +145,12 @@ def get_global_rating(product_name):
             product_url = get_product_url(base_url, product_name)
 
             # Ürün detay sayfasının HTML içeriğini al
-            html_content = get_product_html(product_url)
+            html_content = get_product_detailed_page_html(product_url)
 
             # HTML içeriğinden yıldız bilgisini çıkar
-            rating = extract_rating_from_html(html_content)
+            rating = extract_rating_from_html(html_content, base_url)
             results[base_url] = {"rating": rating}
         except Exception as e:
             results[base_url] = {"error": str(e)}
 
     return results
-
-# resmi on işle
-def preprocess_image(image_path):
-    """Görseli OCR için işler."""
-    image = cv2.imread(image_path)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # Kenarları daha belirgin hale getirmek için eşikleme
-    _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
-
-    return thresh
